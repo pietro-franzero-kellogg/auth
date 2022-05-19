45b276dcb24f7582fb22bf6254182af5c846b53f
pietro-franzero-kellogg <pietro.franzero@kellogg.northwestern.edu>
2022-05-19 02:30:39 +0000

    Adding solutions for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,22 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.find_by({"user_id" => session["user_id"]})
     @task = Task.new
   end
 
   def create
     @task = Task.new
     @task["description"] = params["task"]["description"]
+    @task["user_id"] = @current_user["id"]
     @task.save
     redirect_to "/tasks"
   end
 
   def destroy
     @task = Task.find_by({ "id" => params["id"] })
-    @task.destroy
+    if @current_user["id"] = @tasks["user_id"]
+      @task.destroy
+    end
     redirect_to "/tasks"
   end
 end
